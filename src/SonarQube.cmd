97f3dd0edf2a8be47d3e9606982a969b87a3c6a0
Michael Fink <michael.fink@asamnet.de>
2017-08-02 21:35:16 +0200

    added SonarQube build script

---
+++ src/SonarQube.cmd
@@ -0,0 +1,33 @@
+@echo off
+REM
+REM ulib - a collection of useful classes
+REM Copyright (C) 2000-2017 Michael Fink
+REM
+REM Runs SonarQube analysis build
+REM
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+
+REM set this to your SonarQube tools folder
+set SONARQUBE=D:\devel\tools\SonarQube
+
+PATH=%PATH%;%SONARQUBE%\build-wrapper-win-x86;%SONARQUBE%\sonar-scanner-msbuild
+
+REM
+REM Build using SonarQube scanner for MSBuild
+REM
+rmdir .\bw-output /s /q 2> nul
+
+SonarQube.Scanner.MSBuild.exe begin ^
+    /k:"UlibCpp" ^
+    /v:"4.0.0" ^
+    /d:"sonar.cfamily.build-wrapper-output=%CD%\bw-output" ^
+    /d:"sonar.host.url=https://sonarqube.com" ^
+    /d:"sonar.organization=vividos-github" ^
+    /d:"sonar.login=c59d3403d04744579f89e55e23c27a171293577e"
+
+build-wrapper-win-x86-64.exe --out-dir bw-output msbuild UlibCpp.sln /m /property:Configuration=Release /property:Platform=x86 /target:Rebuild
+
+SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=c59d3403d04744579f89e55e23c27a171293577e"
+
+pause
