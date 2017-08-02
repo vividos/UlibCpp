1e471ae39c99b5d88e0a2f833ef3c3f9d7af9206
Michael Fink <michael.fink@asamnet.de>
2017-08-02 23:36:11 +0200

    added unit test for Exception class

---
+++ src/test/TestException.cpp
@@ -0,0 +1,87 @@
+//
+// ulib - a collection of useful classes
+// Copyright (C) 2006,2007,2008,2009,2012,2017 Michael Fink
+//
+/// \file TestException.cpp tests for Exception class
+//
+
+// includes
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Exception.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+   /// tests Exception class
+   TEST_CLASS(TestException)
+   {
+   public:
+      /// Tests default ctor
+      TEST_METHOD(TestDefaultCtor)
+      {
+         Exception e1(__FILE__, __LINE__);
+         Exception e2(_T("my message"), __FILE__, __LINE__);
+      }
+
+      /// tests throwing and catching by catch(...)
+      TEST_METHOD(TestCatchingByCatchAll)
+      {
+         try
+         {
+            throw Exception(__FILE__, __LINE__);
+            Assert::Fail(_T("must throw exception"));
+         }
+         catch (...)
+         {
+            return;
+         }
+      }
+
+      /// tests if Exception is derived from std::exception
+      TEST_METHOD(TestDerivedByStdException)
+      {
+         try
+         {
+            throw Exception(__FILE__, __LINE__);
+         }
+         catch (const std::exception& ex)
+         {
+            ex;
+            // ok
+         }
+#pragma warning(disable: 4286) // 'const Exception &': is caught by base class ('const std::exception &') on line xx
+         catch (const Exception& ex)
+         {
+            ex;
+            Assert::Fail(_T("must be caught by std::exception catch clause"));
+         }
+         catch (...)
+         {
+            Assert::Fail(_T("must be caught by std::exception catch clause"));
+         }
+      }
+
+      /// tests rethrowing exception
+      TEST_METHOD(TestRethrowException)
+      {
+         try
+         {
+            try
+            {
+               throw Exception(__FILE__, __LINE__);
+            }
+            catch (const Exception& ex)
+            {
+               throw ex;
+            }
+         }
+         catch (const Exception& ex)
+         {
+            ex;
+            return;
+         }
+      }
+   };
+} // namespace UnitTest
