d8c1ee3a02382787ecbdb71e7d07072359bd74d0
Michael Fink <michael.fink@asamnet.de>
2017-08-04 16:15:14 +0200

    added running unit tests and uploading them to SonarQube

--- src/SonarQube.cmd
+++ src/SonarQube.cmd
@@ -24,9 +24,19 @@ SonarQube.Scanner.MSBuild.exe begin ^
     /d:"sonar.cfamily.build-wrapper-output=%CD%\bw-output" ^
     /d:"sonar.host.url=https://sonarqube.com" ^
     /d:"sonar.organization=vividos-github" ^
-    /d:"sonar.login=c59d3403d04744579f89e55e23c27a171293577e"
+    /d:"sonar.login=c59d3403d04744579f89e55e23c27a171293577e" ^
+    /d:sonar.cs.vstest.reportsPaths="%CD%\TestResults\*.trx"
 
+REM
+REM Build all
+REM
 build-wrapper-win-x86-64.exe --out-dir bw-output msbuild UlibCpp.sln /m /property:Configuration=Release /property:Platform=x86 /target:Rebuild
+
+REM
+REM Run unit tests
+REM
+vstest.console.exe /Platform:x86 /InIsolation /logger:trx ^
+   "..\bin\Release\Win32\test\test.dll"
 
 SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=c59d3403d04744579f89e55e23c27a171293577e"
 
