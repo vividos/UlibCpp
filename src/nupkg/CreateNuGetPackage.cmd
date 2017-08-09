bbbb3252fa510d22ce5d748f3dd4571b18506af3
Michael Fink <michael.fink@asamnet.de>
2017-08-09 23:37:57 +0200

    added creating NuGet package for ulib

---
+++ src/nupkg/CreateNuGetPackage.cmd
@@ -0,0 +1,9 @@
+@echo off
+REM
+REM ulib - a collection of useful classes
+REM Copyright (C) 2000-2017 Michael Fink
+REM
+REM Creates NuGet package from header and compiled binaries
+REM
+
+nuget pack
