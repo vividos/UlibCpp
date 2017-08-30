773314fd25d5438c00b5878399ab6d609dcf0082
Michael Fink <michael.fink@asamnet.de>
2017-08-30 10:31:17 +0200

    added msbuild project file to build all configurations and platforms

--- src/BuildNuGetPackage.cmd
+++ src/BuildNuGetPackage.cmd
@@ -11,10 +11,7 @@ call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\V
 REM
 REM Build all
 REM
-msbuild UlibCpp.sln /m /property:Configuration=Debug   /property:Platform=x86 /target:Rebuild
-msbuild UlibCpp.sln /m /property:Configuration=Release /property:Platform=x86 /target:Rebuild
-msbuild UlibCpp.sln /m /property:Configuration=Debug   /property:Platform=x64 /target:Rebuild
-msbuild UlibCpp.sln /m /property:Configuration=Release /property:Platform=x64 /target:Rebuild
+msbuild Build.proj /m /target:Build
 
 cd nupkg
 call CreateNuGetPackage.cmd
