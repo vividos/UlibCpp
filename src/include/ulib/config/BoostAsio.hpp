6f157261605149fca9109c899fb2c3bc34451d63
Michael Fink <michael.fink@asamnet.de>
2017-09-02 08:38:08 +0200

    removed some disabled /analyze warnings that don't appear in latest Boost anymore

--- src/include/ulib/config/BoostAsio.hpp
+++ src/include/ulib/config/BoostAsio.hpp
@@ -13,12 +13,9 @@
 // asio but no winsock init
 #define BOOST_ASIO_DETAIL_IMPL_WINSOCK_INIT_IPP
 
-// ignore prefast warnings in Boost.Asio header files
+// ignore /analyze warnings in Boost.Asio header files
 #pragma warning(push)
-#pragma warning(disable: 6001 6011 6031 6255 6258 6386 6387)
-
-// Boost.Asio in version 1.59 outputs a warning when compiling with VS 2015
-#pragma warning(disable: 4005) // 'BOOST_ASIO_ERROR_CATEGORY_NOEXCEPT': macro redefinition
+#pragma warning(disable: 6001 6031 6255 6258 6386 6387)
 
 // includes
 #include <boost/asio.hpp>
@@ -28,7 +25,7 @@
 // asio but no winsock init
 
 /// \cond false
-inline void boost::asio::detail::winsock_init_base::startup(boost::asio::detail::winsock_init_base::data &,unsigned char,unsigned char){}
-inline void boost::asio::detail::winsock_init_base::cleanup(boost::asio::detail::winsock_init_base::data &){}
-inline void boost::asio::detail::winsock_init_base::throw_on_error(boost::asio::detail::winsock_init_base::data &){}
+inline void boost::asio::detail::winsock_init_base::startup(boost::asio::detail::winsock_init_base::data &, unsigned char, unsigned char) {}
+inline void boost::asio::detail::winsock_init_base::cleanup(boost::asio::detail::winsock_init_base::data &) {}
+inline void boost::asio::detail::winsock_init_base::throw_on_error(boost::asio::detail::winsock_init_base::data &) {}
 /// \endcond
