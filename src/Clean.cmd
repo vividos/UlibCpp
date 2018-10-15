34b644af3125b4ae4f7fbcde379ea3a3b60b1828
Michael Fink <michael.fink@asamnet.de>
2018-10-15 07:48:09 +0200

    added clean script

---
+++ src/Clean.cmd
@@ -0,0 +1,37 @@
+@echo off
+REM
+REM ulib - a collection of useful classes
+REM Copyright (C) 2000-2018 Michael Fink
+REM
+REM Cleans project folders for archiving
+REM
+
+REM set this to your Visual Studio installation folder
+set VSINSTALL=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community
+
+REM
+REM Preparations
+REM
+call "%VSINSTALL%\Common7\Tools\VsDevCmd.bat"
+
+REM
+REM Cleaning
+REM
+msbuild UlibCpp.sln /m /property:Configuration=Debug /property:Platform=x86 /target:Clean
+msbuild UlibCpp.sln /m /property:Configuration=Debug /property:Platform=x86 /target:Clean
+msbuild UlibCpp.sln /m /property:Configuration=Release /property:Platform=x64 /target:Clean
+msbuild UlibCpp.sln /m /property:Configuration=Release /property:Platform=x64 /target:Clean
+
+rmdir .\bw-output /s /q
+rmdir .\.sonarqube /s /q
+rmdir CoverageReport /s /q
+rmdir TestResults /s /q
+rmdir packages /s /q
+rmdir doxygen\obj /s /q
+rmdir .\cov-int /s /q
+del cov-int.zip
+
+rmdir ..\bin /s /q
+rmdir ..\intermediate /s /q
+
+pause
