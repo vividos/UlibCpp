fcf806c9387d82d4bf526b53eb7dc54448c41f99
Michael Fink <michael.fink@asamnet.de>
2019-01-30 07:19:16 +0100

    added ErrorMessage implementation for Android

--- src/ulib/win32/ErrorMessage.cpp
+++ src/ulib/win32/ErrorMessage.cpp
@@ -1,6 +1,6 @@
 //
 // ulib - a collection of useful classes
-// Copyright (C) 2008,2012,2014,2017 Michael Fink
+// Copyright (C) 2008,2012,2014,2017,2019 Michael Fink
 //
 /// \file ErrorMessage.cpp Win32 error message
 //
@@ -8,8 +8,13 @@
 #include "stdafx.h"
 #include <ulib/win32/ErrorMessage.hpp>
 
+#ifdef __ANDROID__
+#include <cerrno>
+#endif
+
 CString Win32::ErrorMessage::ToString()
 {
+#ifdef WIN32
    LPVOID messageBuffer = nullptr;
    ::FormatMessage(
       FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
@@ -26,11 +31,26 @@ CString Win32::ErrorMessage::ToString()
    }
 
    errorMessage.TrimRight(_T("\r\n"));
+#endif
+
+#ifdef __ANDROID__
+   CString errorMessage;
+
+   errorMessage.Format(_T("Error %i (%hs)"),
+      static_cast<int>(m_errorCode),
+      strerror(static_cast<int>(m_errorCode)));
+#endif
 
    return errorMessage;
 }
 
 DWORD Win32::ErrorMessage::LastError()
 {
+#ifdef WIN32
    return GetLastError();
+#endif
+
+#ifdef __ANDROID__
+   return static_cast<DWORD>(errno);
+#endif
 }
