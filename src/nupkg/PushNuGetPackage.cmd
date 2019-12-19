7af10be31d28edc6477fbff0eb0e9bab9ad5ba63
Michael Fink <michael.fink@asamnet.de>
2019-12-19 23:42:23 +0100

    added command script to push built .nupkg file to NuGet.org

---
+++ src/nupkg/PushNuGetPackage.cmd
@@ -0,0 +1,16 @@
+@echo off
+REM
+REM ulib - a collection of useful classes
+REM Copyright (C) 2000-2019 Michael Fink
+REM
+REM Pushes NuGet package to NuGet.org
+REM
+
+if "%NUGET_APIKEY%" == "" echo "Environment variable NUGET_APIKEY is not set! Obtain a new API key under and set the environment variable!"
+if "%NUGET_APIKEY%" == "" echo "Note: API keys are managed under https://www.nuget.org/account/apikeys"
+if "%NUGET_APIKEY%" == "" exit 1
+
+nuget setApiKey %NUGET_APIKEY%
+nuget push Vividos.UlibCpp.Static.4.1.7.nupkg -Source https://www.nuget.org/api/v2/package 
+
+pause
