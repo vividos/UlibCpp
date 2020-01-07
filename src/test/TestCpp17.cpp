581006d0c40fb83d5e6f6d35b66a0c940bd08b47
Michael Fink <michael.fink@asamnet.de>
2020-01-07 21:14:38 +0100

    added compile tests for including all headers using C++17

---
+++ src/test/TestCpp17.cpp
@@ -0,0 +1,9 @@
+//
+// ulib - a collection of useful classes
+// Copyright (C) 2018 Michael Fink
+//
+/// \file TestCpp17.cpp tests compiling all headers with C++17
+//
+
+#include "stdafx.h"
+#include <ulib/ulib.hpp>
