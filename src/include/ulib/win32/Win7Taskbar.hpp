783a40fbcb790907f4f728b22c69c07047bf1d43
Michael Fink <michael.fink@asamnet.de>
2020-01-23 18:47:00 +0100

    removed deleted TaskbarProgressBar ctors and assignment operatos so that it can be stored in std::optional

--- src/include/ulib/win32/Win7Taskbar.hpp
+++ src/include/ulib/win32/Win7Taskbar.hpp
@@ -26,23 +26,11 @@ namespace Win32
          TBPF_PAUSED = 0x8
       };
 
-      /// copy ctor; not available
-      TaskbarProgressBar(const TaskbarProgressBar&) = delete;
-
-      // move ctor; not available
-      TaskbarProgressBar(TaskbarProgressBar&&) = delete;
-
       /// dtor; returns progress to "none"
       ~TaskbarProgressBar()
       {
          SetState(TBPF_NOPROGRESS);
       }
-
-      /// copy assignment operator; not available
-      TaskbarProgressBar& operator=(const TaskbarProgressBar&) = delete;
-
-      /// move assignment operator; not available
-      TaskbarProgressBar& operator=(TaskbarProgressBar&&) = delete;
 
       /// sets new progress bar state
       void SetState(TaskbarProgressBarState state);
