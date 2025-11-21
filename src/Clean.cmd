e6207607acde1767d2daf28364e3a3a556d4defd
Michael Fink <michael.fink@asamnet.de>
2025-11-21 08:15:37 +0100

    chore: simplified the clean cmd script

--- src/Clean.cmd
+++ src/Clean.cmd
@@ -1,26 +1,10 @@
 @echo off
 REM
 REM ulib - a collection of useful classes
-REM Copyright (C) 2000-2022 Michael Fink
+REM Copyright (C) 2000-2025 Michael Fink
 REM
 REM Cleans project folders for archiving
 REM
-
-REM set this to your Visual Studio installation folder
-set VSINSTALL=%ProgramFiles%\Microsoft Visual Studio\2022\Community
-
-REM
-REM Preparations
-REM
-call "%VSINSTALL%\Common7\Tools\VsDevCmd.bat"
-
-REM
-REM Cleaning
-REM
-msbuild UlibCpp.sln /m /property:Configuration=Debug /property:Platform=x86 /target:Clean
-msbuild UlibCpp.sln /m /property:Configuration=Debug /property:Platform=x86 /target:Clean
-msbuild UlibCpp.sln /m /property:Configuration=Release /property:Platform=x64 /target:Clean
-msbuild UlibCpp.sln /m /property:Configuration=Release /property:Platform=x64 /target:Clean
 
 rmdir .vs /s /q
 rmdir .\bw-output /s /q
